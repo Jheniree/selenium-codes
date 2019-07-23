4c4e70c5d2cb9e6255cfbb976a17e9c938261684
Carlos Lens <Carlos@iMac-de-Carlos.local>
2019-07-23 20:56:53 +0200

    First commit - initial structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import example.ExampleObject;
+
+public class Main {
+
+
+    ExampleObject exampleObject = new ExampleObject();
+
+    public void Prueba(){
+        exampleObject.testMyExampleObject("PERRO");
+    }
+}
+
+
---
+++ src/test/java/amazon.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class amazon {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://wikipedia.org");
+        long miliSeconds = 3000;
+
+        Thread.sleep(miliSeconds);
+        WebElement element = driver.findElement(By.id("js-link-box-en"));
+        element.click();
+
+
+       Thread.sleep(miliSeconds);
+        WebElement searchBox;
+        searchBox = driver.findElement(By.id("searchInput"));
+        searchBox.sendKeys("Software");
+        Thread.sleep(miliSeconds);
+        WebElement element1 = driver.findElement(By.id("searchButton"));
+        element1.click();
+        Thread.sleep(miliSeconds);
+
+        Actions action = new Actions(driver);
+        WebElement perro = driver.findElement(By.linkText("History of software"));
+        Thread.sleep(miliSeconds);
+        action.moveToElement(perro).perform();
+        Thread.sleep(miliSeconds);
+
+
+
+
+       //articulo = driver.find_element_by_xpath("///div/div[1]/div/div[4]/a[1]/i");
+
+        driver.quit();
+
+    }
+}
